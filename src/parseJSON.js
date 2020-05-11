a2862458682cadb1eb1512063684d45b14952288
Kim Dang <kimdang229@gmail.com>
2020-05-11 16:45:18 -0700

    Implemented parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,28 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json[0] === "{") {
+    parseObject(json);
+  }
+  // object - begins with {
+  function parseObject(obj) {
+    var outputObj = {};
+    obj = obj.slice(1, obj.length-1);
+    var arrObj = obj.split(":"); // ["name", "6"];
+    for (var i = 0; i < arrObj.length; i++) {
+      if (i % 2 === 0) { // "name"
+        parseString(arrObj[i]);
+      }
+    }
+  }
+  // array - begins with [
+  // string - begins with "
+  function parseString(str) {
+
+  }
+  // number - starts with number
+  // boolean - begins with f or t, no quote
+
 };
+
+console.log(parseJSON(`{"name":"6"}`)); // => {"name":6}
