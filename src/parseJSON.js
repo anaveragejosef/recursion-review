830dd95e840a896c9715b24b72adef90b06ef4bf
Kim Dang <kimdang229@gmail.com>
2020-05-11 17:14:25 -0700

    Fix implementation

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -6,17 +6,36 @@ var parseJSON = function(json) {
   // your code goes here
   if (json[0] === "{") {
     parseObject(json);
+  } else if (json[0] === "[") {
+    parseArray(json);
+  } else if (json[0] === /[a-z]|[A-Z]/) {
+    parseString(json);
+  } else if (json[0] === /[0-9]|-|./) {
+    parseNumber(json);
   }
+    else if (json === 'true'){
+      return true;
+  } else if (json === 'false'){
+    return false;
+  } else if (json === 'null'){
+    return null;
+  }
+
   // object - begins with {
   function parseObject(obj) {
     var outputObj = {};
     obj = obj.slice(1, obj.length-1);
-    var arrObj = obj.split(":"); // ["name", "6"];
+    var arrObj = obj.split(","); // ["name", "6"], [age:"6"];
+
     for (var i = 0; i < arrObj.length; i++) {
-      if (i % 2 === 0) { // "name"
-        parseString(arrObj[i]);
-      }
+      var pair = arrObj[i].split(":");
+      var key = pair[0];
+      var value = pair[1];
+      parseJSON(value);
+      console.log(value);
     }
+
+
   }
   // array - begins with [
   // string - begins with "
@@ -28,4 +47,4 @@ var parseJSON = function(json) {
 
 };
 
-console.log(parseJSON(`{"name":"6"}`)); // => {"name":6}
+console.log(parseJSON(`{"name":"kim", "age":"6"}`)); // => {"name":"kim", "age":6}
